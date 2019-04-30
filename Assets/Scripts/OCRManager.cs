b20e077d7602344b08431ca181f01a043c6c8367
Alexandr Dobryak <singldraft@gmail.com>
2019-04-30 15:23:56 +0300

    first commit

---
+++ Assets/Scripts/OCRManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Tesseract;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OCRManager : MonoBehaviour
+{    
+    [SerializeField]
+    public Text resultText;
+    string dataPath;
+
+    void Start()
+    {
+        dataPath = Path.Combine(Application.streamingAssetsPath, "tessdata");
+    }
+
+    public string GetText(string path)
+    {
+        var ocr = new TesseractEngine(dataPath, "rus", EngineMode.Default);
+        Pix p = Pix.LoadFromFile(path);
+        var page = ocr.Process(p);
+        var result = page.GetText().Split(' ', '\n')[0];
+        resultText.text = result;
+        return result;
+    }
+}
---
+++ Assets/Scripts/WebCamPhotoCamera.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class WebCamPhotoCamera : MonoBehaviour
+{
+    WebCamTexture webCamTexture;
+    [SerializeField]
+    RawImage rICamera;
+    [SerializeField]
+    GameObject rILastPhotoHolder;
+    Texture2D curPhoto;
+
+    [SerializeField]
+    OCRManager ocrManager;
+
+    string path;
+
+    void Start()
+    {
+        ocrManager = GetComponent<OCRManager>();
+        WebCamDevice[] devices = WebCamTexture.devices;
+        path = Application.streamingAssetsPath;
+        webCamTexture = new WebCamTexture(devices[0].name);
+        GetComponent<Renderer>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+        print(path + @"/photo.jpg");
+    }
+
+    void Update()
+    {
+        rICamera.texture = webCamTexture;
+    }
+
+    public void Photo()
+    {
+        StartCoroutine(TakePhoto());
+    }
+
+    public void ShowPhoto()
+    {
+        rILastPhotoHolder.GetComponent<RawImage>().texture = curPhoto;
+    }
+
+    IEnumerator TakePhoto()
+    {
+        yield return new WaitForEndOfFrame();
+        
+        Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+        photo.SetPixels(webCamTexture.GetPixels());
+        photo.Apply();
+        byte[] bytes = photo.EncodeToPNG();
+        File.WriteAllBytes(Path.Combine(path, "photo.png"), bytes);
+        ocrManager.GetText(Path.Combine(path, "photo.png"));
+        curPhoto = photo;
+    }
+}
